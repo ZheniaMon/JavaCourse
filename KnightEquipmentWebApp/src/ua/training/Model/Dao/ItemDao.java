3db2179e80ff8b930c3f0397d0e8c9b1b36c1be7
ZheniaMon <zshmakov@gmail.com>
2017-12-25 15:11:06 +0200

    update

--- KnightEquipmentWebApp/src/ua/training/Model/Dao/ItemDao.java
+++
@@ -1,7 +0,0 @@
-package ua.training.Model.Dao;
-
-import ua.training.Model.Enity.EquipmentItem;
-
-public interface ItemDao extends GenericDao<EquipmentItem> {
-
-}
--- KnightEquipmentWebApp/src/ua/training/Model/Dao/KnightDao.java
+++
@@ -1,6 +0,0 @@
-package ua.training.Model.Dao;
-
-import ua.training.Model.Enity.Knight;
-
-public interface KnightDao extends GenericDao<Knight> {
-}
